a87809af960f24738f3f1c43724f871749219229
user <user@WIN-1H53FEO9J8J>
2023-06-25 12:24:51 +0300

    Add project files.

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const oats = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ oats.abilities = {
+  "str": "oats.AbilityStr",
+  "dex": "oats.AbilityDex",
+  "con": "oats.AbilityCon",
+  "int": "oats.AbilityInt",
+  "wis": "oats.AbilityWis",
+  "cha": "oats.AbilityCha"
+};
+
+oats.abilityAbbreviations = {
+  "str": "oats.AbilityStrAbbr",
+  "dex": "oats.AbilityDexAbbr",
+  "con": "oats.AbilityConAbbr",
+  "int": "oats.AbilityIntAbbr",
+  "wis": "oats.AbilityWisAbbr",
+  "cha": "oats.AbilityChaAbbr"
+};
